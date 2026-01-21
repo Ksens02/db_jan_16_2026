1a17a6c46da8fb65c31292d22bdf469d0bf2c8f0
Keller-Sens <200811951+Ksens02@users.noreply.github.com>
2026-01-21 17:58:16 +0000

    updated fetch.py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # db_jan_16_2026
-CIS 350 database related to nfl playoffs. Practicing adding information into the database and pulling it out and printing in the terminal.
+CIS 350 database related to nfl playoffs. Practicing the following: adding information into the database, fetching the information, and printing in the records in the terminal.
--- fetch.py
+++ fetch.py
@@ -1,8 +1,14 @@
 import sqlite3
+import pandas as pd
 
 conn = sqlite3.connect('nfl_playoffs.db')
-
 cursor = conn.cursor()
+
+# Code below selects all the records from the points table
+# query = """
+#     SELECT * 
+#     FROM points;
+# """
 
 query = """
     SELECT * 
@@ -10,7 +16,28 @@ query = """
 """
 
 cursor.execute(query)
-results = cursor.fetchall()
+records = cursor.fetchall()
 conn.close()
+# print(records)
 
-print(results)
+# The lines below print out the contents of the database in a dataframe
+# records_df = pd.DataFrame(records, columns = ['id', 'city', 'name'])
+# print(records_df)
+
+# PRINT OUT LIST OF CITIES
+# list_of_city_names = []
+# for record in records:
+#     list_of_city_names.append(record[1])
+
+
+# PRINT OUT THE LENGTH OF EACH CITY IN A LIST
+# lengths = []
+# for record in records:
+#     lengths.append(len(record[1]))
+
+# print(lengths)
+
+lengths = []
+for record in records:
+    lengths.append(len(record[1], record[2]))
+print(lengths)
